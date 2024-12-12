5dd74dc1301d89ff1e84590b7ad564c4f81ee858
Sam Crouse <scrouse2@uwyo.edu>
2024-12-12 11:49:06 -0700

    init commit of files

---
+++ CountNuclei.py
@@ -0,0 +1,192 @@
+"""
+Documentation
+Author: Samuel Crouse
+Lab: WSBC Sun Lab
+
+"""
+
+# note: import statements
+from PIL import Image
+import pathlib
+import math
+
+
+# loads an image and converts it to grayscale
+def loadImageGrayscale(filePath, savePath=None):
+    img = Image.open(filePath).convert('L')
+
+    if savePath is not None:
+        img.save(savePath)
+
+    return img
+
+
+# normalizes a grayscale image
+def normalizeImage(img, normalToImg, savePath=None):
+    pix = img.load()
+    width = normalToImg.width
+    height = normalToImg.height
+
+    # get the scale values from the normalToImg
+    minPix = 256
+    maxPix = 0
+    for y in range(height):
+        for x in range(width):
+            gray = pix[x, y]
+            if gray < minPix:
+                minPix = gray
+
+            if gray > maxPix:
+                maxPix = gray
+
+    img = img.copy()  # don't modify input image
+
+    pix = img.load()
+    width = img.width
+    height = img.height
+
+    # scale the input img to the normalToImg
+    for y in range(height):
+        for x in range(width):
+            gray = pix[x, y]
+            zi = math.floor(((gray - minPix) / (maxPix - minPix)) * 255)
+            pix[x, y] = zi
+
+    if savePath is not None:
+        img.save(savePath)
+
+    return img
+
+
+# note: counts the nuclei in the image based on a threshold grayscale value
+# threshold and lowerThreshold are inclusive bounds
+def countNuclei(img, upperThreshold, lowerThreshold=None, nucleiPixelWidth=5, mark=False, simple=False, verySimple=False, savePath=None, saveCountsToIMG=False):
+    if lowerThreshold is None:
+        lowerThreshold = 0
+
+    if verySimple is True:
+        simple = True
+
+    img = img.copy()
+    pix = img.load()
+    width = img.width
+    height = img.height
+
+    if mark:
+        markedImg = Image.new("RGB", img.size)
+        markedImg.paste(img)
+        markedPix = markedImg.load()
+
+    count = 0
+    markedPixels = []  # column indexes that have been marked
+
+    canMark = True
+    for y in range(height):
+        print("progress: {}%".format(round((y / len(range(height))) * 100, 4)))
+        # update the marked pixels
+        newMarkedPixels = []
+        for index in markedPixels:
+            if y < index[1] + nucleiPixelWidth:
+                newMarkedPixels.append(index)
+        markedPixels = newMarkedPixels
+
+        for x in range(width):
+            if simple:
+                markedPixels = []
+
+            # determine if we are far enough away from other nuclei
+            else:
+                canMark = True
+                for index in markedPixels:
+                    # check x and y
+                    if (index[0] - nucleiPixelWidth) < x < (index[0] + nucleiPixelWidth):
+                        if (index[1] - nucleiPixelWidth) < y < (index[1] + nucleiPixelWidth):
+                            canMark = False
+                            break
+
+            if verySimple:
+                canMark = True
+
+            gray = pix[x, y]
+            if canMark:
+                if lowerThreshold <= gray <= upperThreshold:
+                    if simple:
+                        canMark = False
+
+                    markedPixels.append((x, y))
+
+                    count += 1
+                    if mark is True:
+                        markedPix[x, y] = (255, 0, 0)
+
+            if simple:
+                if gray > upperThreshold or gray < lowerThreshold:
+                    canMark = True
+
+    if savePath is not None and mark is True:
+        if saveCountsToIMG is True:
+            endIndex = savePath.index('.')
+            countString = "_" + str(count)
+            savePath = savePath[:endIndex] + countString + savePath[endIndex:]
+
+        markedImg.save(savePath)
+
+    return count
+
+
+# note: code for grayscaling and normalizing a new img
+def prepIMG(imgPath, savePath=None, normalPath=None, log=False):
+    if normalPath is None:
+        normalPath = imgPath
+
+    if log: print("\ngrayscaling image...")
+
+    img_path = imgPath
+    img = loadImageGrayscale(img_path, savePath=savePath)
+    normalToImg = loadImageGrayscale(normalPath)
+
+    if log:
+        print("width: {}, height: {}".format(img.width, img.height))
+        print("width: {}, height: {}".format(normalToImg.width, normalToImg.height))
+
+        print("\nnormalizing image...")
+
+    img = normalizeImage(img=img, normalToImg=normalToImg, savePath=savePath)
+
+    return img
+
+
+def main():
+    print("start tests...")
+    directory = str(pathlib.Path().absolute().resolve()) + "\\images\\"
+    imgNum = 3
+
+    # code for grayscaling and normalizing a new img
+    """
+    img_path = directory + "img{}.png".format(imgNum)
+    print("\ngrayscaling image...")
+    img = loadImageGrayscale(img_path, savePath=directory+"gray{}.png".format(imgNum))
+    normalToImg = loadImageGrayscale(directory + "img1.png")
+
+    print("width: {}, height: {}".format(img.width, img.height))
+    print("width: {}, height: {}".format(normalToImg.width, normalToImg.height))
+
+    print("\nnormalizing image...")
+    img = normalizeImage(img=img, normalToImg=normalToImg, savePath=directory+"gray{}_normal.png".format(imgNum))
+    # """
+
+    # load an already grayscaled and normalized img
+    # """
+    img_path = directory + "gray{}_normal.png".format(imgNum)
+    img = loadImageGrayscale(img_path)
+    # """
+
+    print("\ncounting nuclei...")  # 110 is a pretty good threshold  # 100 is maybe even better for darker images
+    count = countNuclei(img, upperThreshold=120, lowerThreshold=90, nucleiPixelWidth=10, mark=True, simple=False,
+                        verySimple=False, savePath=directory + "gray{}_normal_marked_new_4.png".format(imgNum))
+
+    print("final count:", count)
+
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,65 @@
+"""
+Documentation
+Author: Samuel Crouse
+Lab: WSBC Sun Lab
+
+"""
+
+import CountNuclei as cn
+import pathlib
+
+
+def main():
+    print("grayscaling and counting...")
+    directory = str(pathlib.Path().absolute().resolve()) + "\\images\\Madeline\\"
+    imgNum = 1
+    name = "brain-slice"
+
+    # prep the image by grayscaling and normalizing it
+    """
+    imgName = f"{name}_{imgNum}.PNG"
+    imgPath = directory + imgName
+    graySavePath = directory + f"{name}_gray_{imgNum}.PNG"
+    gray = cn.prepIMG(imgPath=imgPath, savePath=graySavePath, log=True)
+    # """
+
+    # note: load an already grayscaled and normalized img
+    # """
+    grayToLoadPath = directory + f"brain-slice_gray_1.PNG"
+    gray = cn.loadImageGrayscale(grayToLoadPath)
+    # """
+
+    print("\ncounting nuclei and outputting very simple, simple, and normal images...")
+    counts = []
+    for i in range(3):
+        verySimple = True
+        simple = False
+        sType = "VS"
+
+        if i == 1:
+            verySimple = False
+            simple = True
+            sType = "S"
+
+        if i == 2:
+            verySimple = False
+            simple = False
+            sType = "N"
+
+        imgNum = 1  # insert this into the img path if you want to track different images
+        lowThresh = 60
+        upThresh = 200
+        width = 6
+        markedSavePath = directory + f"{name}_marked_{lowThresh}-{upThresh}-{width}-{sType}.PNG"
+        if i != 2: markedSavePath = None
+        count = cn.countNuclei(img=gray, upperThreshold=upThresh, lowerThreshold=lowThresh, nucleiPixelWidth=width,
+                               mark=True, simple=simple, verySimple=verySimple,
+                               savePath=markedSavePath, saveCountsToIMG=True)
+        counts.append(count)
+
+    print("final counts: very simple: {} | simple: {} | normal: {}".format(*counts))
+    # """
+
+
+if __name__ == '__main__':
+    main()
